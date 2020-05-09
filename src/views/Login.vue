18fb92d97c0bbb7edb3b93990309b16bf9a7df31
hotdari <38778044+hotdari@users.noreply.github.com>
2020-05-09 23:48:06 +0900

    login page making, router

---
+++ src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container fill-height style="max-width:450px;">
+    <v-layout align-center row wrap>
+      <v-flex xs12>
+        <v-card>
+          <v-toolbar flat dark class="primary">
+            <v-toolbar-title>로그인</v-toolbar-title>
+          </v-toolbar>
+          <div class="pa-3">
+            <v-text-field v-model="email" label="이메일을 입력하세요.">
+            </v-text-field>
+            <v-text-field
+              v-model="password"
+              label="패스워드를 입력하세요."
+              type="password"
+            >
+            </v-text-field>
+          </div>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      email: null,
+      password: null
+    }
+  }
+}
+</script>
